f1927da416ceec044b481e31b92f9aa66204dace
Gary Bortosky <garybort@gmail.com>
2017-07-22 12:55:04 -0500

    update info

--- README.md
+++ README.md
@@ -4,3 +4,31 @@
 
 # gb-random
 An alternative implementation of Math.random using crypto.getRandomBytes
+
+## Usage
+
+> npm install gb-random --save
+
+```
+const gbrandom = require("gb-random");
+
+const rand = new gbrandom.Random();
+
+// just replace Math.random with rand.getRandomSync
+// console.log(Math.random(), "Math.random()");
+console.log(rand.getRandomSync(), "gb-random");
+
+// you can also use async
+rand.getRandom().then(random => {
+  console.log(random); // 0.3879
+});
+
+// or even better in an async function
+const rando = await rnd.getRandom();
+console.log(rando);
+
+rand.getRandoms(3).then(randoms => {
+  console.log(randoms); // [ 0.3879, 0.9659, 0.3019 ]
+});
+```
+
--- demo.js
+++ demo.js
@@ -1,21 +1,26 @@
-"use strict";
 const gbrandom = require("./lib/random");
 
-const NaNValue = NaN;
 const rand = new gbrandom.Random();
-const nanRand = new gbrandom.Random({ NaNValue });
-
-const res = rand.floatFromBuffer(2, Buffer.from([1, 2, 3, 4, 5, 6, 7, 8]));
-console.log(res, "sync"); // [ 0.387939260590663, 0.301941157072183 ]
-
-nanRand.getRandoms(3).then(randoms => {
-  console.log(randoms); // [ 0.387939260590663, NaN, 0.301941157072183 ]
-});
 
 rand.getRandom().then(random => {
-  console.log(random); // 0.387939260590663
+  console.log(random); // 0.3879
 });
 
-console.log(rand.getRandomSync(), "sync"); // 0.387939260590663
+rand.getRandoms(3).then(randoms => {
+  console.log(randoms); // [ 0.3879, 0.9659, 0.3019 ]
+});
+
+console.log(Math.random(), "Math.random()");
+console.log(rand.getRandomSync(), "gb-random");
+
+/*
+some stuff here
+*/
+
+const nanRand = new gbrandom.Random({ NaNValue });
+const NaNValue = NaN;
+
+const res = rand.floatFromBuffer(2, Buffer.from([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(res, "sync"); // [ 0.3879, 0.3019 ]
 
 console.log(gbrandom.NanBuffer); // [ 127, 192, 0, 1 ]
